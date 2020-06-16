3494055eff4287a1c84b087dc831518c2371c5a7
Mr.Leon <levan.melkonian@gmail.com>
2020-06-16 20:23:16 +0300

    app, functional

---
+++ app.py
@@ -0,0 +1,18 @@
+import sys
+from PyQt5 import QtWidgets
+import design
+
+
+class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    window = App()
+    window.show()
+    app.exec_()
+
+if __name__ == '__main__':
+    main()
