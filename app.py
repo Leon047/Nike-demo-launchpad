355d3d2ff896cab4c447121d2afb79f74b127830
Mr.Leon <levan.melkonian@gmail.com>
2020-06-24 13:38:17 +0300

    app, design, #

--- app.py
+++ app.py
@@ -7,36 +7,38 @@ import request_data
 import design
 
 class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
-
     def __init__(self):
         super().__init__()
         self.setupUi(self)
 
     def base_image(self):
-        self.photo = ClickableLabel()
-        self.photo.setPixmap(QtGui.QPixmap("image/image0.jpg"))
-        self.photo.clicked.connect(label_clicked)
+        # Basic definitions of photo
+        self.photo = ClickableLabel(self)
+        self.photo_link=self.photo.setPixmap(QtGui.QPixmap("image/image0.jpg"))
+        self.photo.clicked.connect(self.label_clicked)
 
-        self.photo_1 = ClickableLabel()
-        self.photo_1.setPixmap(QtGui.QPixmap("image/image1.jpg"))
-        self.photo_1.clicked.connect(label_clicked)
+        self.photo_1 = ClickableLabel(self)
+        self.photo1_link=self.photo_1.setPixmap(QtGui.QPixmap("image/image1.jpg"))
+        self.photo_1.clicked.connect(self.label_clicked)
 
-        self.photo_2 = ClickableLabel()
-        self.photo_2.setPixmap(QtGui.QPixmap("image/image2.jpg"))
-        self.photo_2.clicked.connect(label_clicked)
+        self.photo_2 = ClickableLabel(self)
+        self.photo2_link=self.photo_2.setPixmap(QtGui.QPixmap("image/image2.jpg"))
+        self.photo_2.clicked.connect(self.label_clicked)
 
-        self.photo_3 = ClickableLabel()
-        self.photo_3.setPixmap(QtGui.QPixmap("image/image3.jpg"))
-        self.photo_3.clicked.connect(label_clicked)
+        self.photo_3 = ClickableLabel(self)
+        self.photo3_link=self.photo_3.setPixmap(QtGui.QPixmap("image/image3.jpg"))
+        self.photo_3.clicked.connect(self.label_clicked)
 
-        self.photo_4 = ClickableLabel()
-        self.photo_4.setPixmap(QtGui.QPixmap("image/image4.jpg"))
-        self.photo_4.clicked.connect(label_clicked)
+        self.photo_4 = ClickableLabel(self)
+        self.photo4_link=self.photo_4.setPixmap(QtGui.QPixmap("image/image4.jpg"))
+        self.photo_4.clicked.connect(self.label_clicked)
 
     def query_image(self):
-        self.photo_5.setPixmap(QtGui.QPixmap("image/image5.jpg"))
+        # Target photo
+        self.photo5_link=self.photo_5.setPixmap(QtGui.QPixmap("image/image5.jpg"))
 
     def found_images():
+        # Basic definitions for found photos
         self.photo_6 = ClickableLabel()
         self.photo_6.setPixmap(QtGui.QPixmap("image/image6.jpg"))
         self.photo_6.clicked.connect(label_clicked)
@@ -85,16 +87,24 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
         self.pushButton.setText(_translate("MainWindow", "SEARCH"))
         self.pushButton.clicked.connect(search_pushbutton_clicked)
 
+    def label_clicked(self):
+        # Events for clicking on the photo.
+        if self.photo.clicked.connect():
+            self.photo_5 = self.photo
+            self.add_image_url(0)
 
-    def label_clicked(self, source): pass
-        # Events for clicking on the image.
-        # self.textbox.text()
+    def add_image_url(self, url_index):
+        # Overrides URL in the request_data module
+        base_url_data = request_data.base_shoes_url[url_index]
+        request_data.image_url = list(base_url_data)
 
     def add_lineedit_data(self):
+        # Overrides positive and negative atribuds in the request_data module
         request_data.add_positive_attributes.append(self.positive_text)
         request_data.add_negative_attributes.append(self.negative_lineedit)
 
     def add_combobox_data(self):
+        # Overrides the values of the Gender string in the request_data module
         if self.comboBox ==  "All":
             request_data.gender = "All"
         elif self.comboBox ==  "Men":
@@ -105,6 +115,7 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
             request_data.gender = "Unisex"
 
     def add_combobox1_data(self):
+        # Overrides the values of the Item Type string in the request_data module
         if self.comboBox_1 == "Sports":
             request_data.sport = "True"
             request_data.casual = "False"
@@ -114,9 +125,6 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
         else:
             request_data.casual = "False"
             request_data.casual = "False"
-
-    def add_image_url():
-        pass
 
     def search_pushbutton_clicked(self):
         app_requests.create_json()
@@ -131,7 +139,7 @@ class App(QtWidgets.QMainWindow, design.Ui_MainWindow):
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = App()
-    # window.setFixedSize(725, 670)
+    window.setFixedSize(879, 513)
     window.show()
     app_requests.base_request()
     app.exec_()
